5487f7e394ed33aa5bb66cdb87ec9616ae72bd98
Deguta <tech.ym1110@gmail.com>
2020-07-26 12:56:26 +0900

    インクリメンタルサーチ
    complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-  def edit
+  def index
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
-
   def update
     if current_user.update(user_params)
       redirect_to root_path
